bc09a132a7d2730255efeb08d64551312b70e6b7
WekyJay <505083907@qq.com>
2024-10-24 15:07:41 +0800

    HIke数据展示动画添加_20241024

---
+++ Landmarks/Views/HikeView.swift
@@ -0,0 +1,18 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by MacBook on 2024/10/24.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HikeView()
+}
